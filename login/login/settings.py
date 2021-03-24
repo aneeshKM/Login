8a5a0efca6a179a41b6f6f42fb9ea9410b58ac18
hardi-12 <hardi120400@gmail.com>
2021-03-24 18:35:18 +0530

    Final

--- login/login/settings.py
+++ login/login/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'x)8x$f4$*bmnzj3c&ifj6bea-25fxs+933^sj)5r_gg(a8lqbw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
